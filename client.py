61e3f806c291612e207394786191c2cef6e7340d
Shahriar <mehraneftekhari99@gmail.com>
2023-06-29 23:58:03 +0300

    add prompts and help commands to client

--- client.py
+++ client.py
@@ -39,19 +39,49 @@ def send_message(message):
 
 
 def chat():
-    print("Starting the chat, type 'quit' or press Ctrl-D to exit.")
+    print(
+        Fore.LIGHTCYAN_EX
+        + "Starting the chat, type '/help' to show list of commands. Press Ctrl-D to exit."
+        + Style.RESET_ALL
+    )
 
     while True:
         try:
-            user_message = input(Fore.GREEN + "User: " + Style.RESET_ALL)
+            user_message = input(Fore.YELLOW + "User: " + Style.RESET_ALL)
         except EOFError:
             break
 
-        if user_message.lower() == "quit":
-            break
+        if user_message.startswith("/"):
+            handle_command(user_message)
+            continue
 
         response = send_message(user_message)
         print(Fore.BLUE + "Zeus: " + Style.RESET_ALL + response.get("response", ""))
+
+
+def prompts():
+    response = requests.get(f"http://{PANTHEON_SERVER_IP}:{PANTHEON_SERVER_PORT}/prompts")
+    return response.json()
+
+
+def help():
+    print(Fore.GREEN + "/help" + Style.RESET_ALL + " - show this help message")
+    print(Fore.GREEN + "/prompts" + Style.RESET_ALL + " - show list of prompts")
+    print(Fore.GREEN + "/exit (Ctrl-D)" + Style.RESET_ALL + " - exit the chat")
+    print(Fore.GREEN + "You can use arrow keys to navigate the chat history\n" + Style.RESET_ALL)
+
+
+def handle_command(command):
+    if command == "/prompts":
+        print("Available prompts:")
+        for prompt in prompts()["prompts"]:
+            print(f" - {prompt}")
+    elif command == "/help":
+        help()
+    elif command == "/exit":
+        sys.exit(0)
+    else:
+        print(f"Unknown command: {command}")
 
 
 if __name__ == "__main__":
